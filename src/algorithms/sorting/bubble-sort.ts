841eeb7c4354000934d58a7456cd4b657382e72e
Mohamed Farhan <112707832+mdfarhan20@users.noreply.github.com>
2024-10-06 05:48:03 +0000

    optimized bubble sort implementation

--- src/algorithms/sorting/bubble-sort.ts
+++ src/algorithms/sorting/bubble-sort.ts
@@ -1,12 +1,15 @@
 // Repeatedly iterates over the array checking for unsorted consecutive pairs and swaps them
+
 function bubbleSort(array: number[]) {
-  while (true) {
+  // The last element will be in sorted order after each iteration
+  for (let i = array.length - 1; i >= 0; i--) {
     let swapped = false;
-    for (let i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        let temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
+
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
         swapped = true;
       }
     }
@@ -15,6 +18,7 @@ function bubbleSort(array: number[]) {
       break;
   }
 }
+
 
 const numbers = [3, 1, 4, 2, 1, 6, 5];
 bubbleSort(numbers);
